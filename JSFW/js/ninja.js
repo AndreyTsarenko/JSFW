dd97e01ae52845800d1786f2ae07f90485c2e9a8
a.tsarenko <a.tsaernko>
2013-05-05 18:29:27 +0300

    second commit

---
+++ JSFW/js/ninja.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: User
+ * Date: 05.05.13
+ * Time: 18:14
+ * To change this template use File | Settings | File Templates.
+ */
+var start = function () {
+    var js = [
+    ];
+    var css = [
+    ];
+}
